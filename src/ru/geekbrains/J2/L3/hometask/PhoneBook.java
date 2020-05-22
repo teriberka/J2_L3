19d41849cf0ccc918a3c3f8af82efb8074886239
teriberka <box4trash@inbox.ru>
2020-05-22 15:23:00 +0300

    add part 2

--- src/ru/geekbrains/J2/L3/hometask/PhoneBook.java
+++ src/ru/geekbrains/J2/L3/hometask/PhoneBook.java
@@ -4,5 +4,13 @@ import java.util.HashMap;
 
 
 public class PhoneBook {
+    public HashMap<String, HashMap<String, String>> myPhoneBook = new HashMap<>();
 
+    public HashMap<String, String> getInfo(String s){
+        return myPhoneBook.get(s);
+    }
+
+    public void addInfo(String name, HashMap<String, String> record) {
+        myPhoneBook.put(name, record);
+    }
 }
